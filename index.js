69991477f109926db52410c9234543722b2bfab2
ivan nesterenko <nester164ivan@yandex.ru>
2022-09-29 11:34:22 +0300

    made chenges in master

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log('Hello  1 World');
 
 console.log('Hello');
+const userAge = 35;
+if (userAge >= 18) {
+  console.log('Вы совершеннолетний');
+} else {
+  console.log('Вы НЕ совершеннолньний');
+}
